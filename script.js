3b47bcef8fa032cf63711e6247aca151375ac81d
heyysaikat <heyysaikat@gmail.com>
2024-09-01 22:32:59 +0530

    UI changes

--- script.js
+++ script.js
@@ -3,3 +3,32 @@ document.querySelector('#navBtn').addEventListener('click', function() {
         behavior: 'smooth'
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const gymElement = document.querySelector('.gym');
+    
+    gymElement.addEventListener('click', function(event) {
+        event.stopPropagation();
+        gymElement.classList.toggle('active');
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!gymElement.contains(event.target)) {
+            gymElement.classList.remove('active');
+        }
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const zumbaElement = document.querySelector('.zumba');
+    
+    zumbaElement.addEventListener('click', function(event) {
+        event.stopPropagation();
+        zumbaElement.classList.toggle('active');
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!zumbaElement.contains(event.target)) {
+            zumbaElement.classList.remove('active');
+        }
+    });
+});
